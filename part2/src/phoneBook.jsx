6a0b9eafd975b5f1ad32fe0c8dd4519113a9c4a5
bran.nie <pengcheng1995@gmail.com>
2020-08-09 15:28:47 +0800

    part2, 2.1-2.10

---
+++ part2/src/phoneBook.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+
+const Button = ({ text, handleClick }) => (
+    <button onClick={handleClick}>{text}</button>
+);
+const Person = ({ person }) => {
+    const { name, number } = person;
+    return (
+        <p>
+            {name}, number: {number}
+        </p>
+    );
+};
+const Persons = ({ persons }) => {
+    return persons.map((person) => <Person key={person.id} person={person} />);
+};
+
+const PersonForm = (props) => {
+    const { name, nameHandle, number, numberHandle, handleSubmit } = props;
+    return (
+        <form>
+            <div>
+                name: <input value={name} onChange={nameHandle} />
+                <br />
+                number:
+                <input value={number} onChange={numberHandle} />
+            </div>
+            <div>
+                <Button text="add" handleClick={handleSubmit} />
+            </div>
+        </form>
+    );
+};
+
+const FilterPersons = (props) => {
+    const { filterName, handleFilterNameChange, filterPersons } = props;
+    return (
+        <div className="search">
+            <span>filter shown with</span>
+            <input value={filterName} onChange={handleFilterNameChange} />
+            <Persons persons={filterPersons} />
+        </div>
+    );
+};
+
+const PhoneBook = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '1', id: 1 },
+    ]);
+    const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [filterName, setFilterName] = useState('');
+    const [filterPersons, setFilterPersons] = useState([]);
+
+    const resetInput = () => {
+        setNewName('');
+        setNewNumber('');
+    };
+
+    const handleNameChange = (e) => setNewName(e.target.value);
+    const handleNumberChange = (e) => setNewNumber(e.target.value);
+    const handleFilterNameChange = (e) => {
+        const v = e.target.value;
+        setFilterName(v);
+        let filter = [];
+        if (v) {
+            filter = persons.filter((person) => {
+                return person.name.toLowerCase().includes(v.toLowerCase());
+            });
+        }
+        setFilterPersons(filter);
+    };
+    const addPerson = (e) => {
+        e.preventDefault();
+        // 此处用some，效率比较好，some在return true时，就会终止遍历
+        const personExist = persons.some((person) => {
+            return person.name === newName;
+        });
+        if (personExist) {
+            alert(`${newName} is already added to phonebook`);
+            resetInput();
+            return;
+        }
+        const newPerson = {
+            id: persons.length + 1,
+            name: newName,
+            number: newNumber,
+            date: new Date().toISOString(),
+        };
+
+        setPersons(persons.concat(newPerson));
+        resetInput();
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <FilterPersons
+                filterName={filterName}
+                handleFilterNameChange={handleFilterNameChange}
+                filterPersons={filterPersons}
+            />
+            <h3>Add a new</h3>
+            <PersonForm
+                name={newName}
+                nameHandle={handleNameChange}
+                number={newNumber}
+                numberHandle={handleNumberChange}
+                handleSubmit={addPerson}
+            />
+            <h3>Persons</h3>
+            <Persons persons={persons} />
+        </div>
+    );
+};
+
+export default PhoneBook;
