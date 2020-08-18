00c0aadd518e339d6e0b026bbeaa5789c5cc57c3
bran.nie <pengcheng1995@gmail.com>
2020-08-18 18:52:33 +0800

    单元测试；serverless部署

---
+++ part4/app/sls.js
@@ -0,0 +1,3 @@
+const app = require('./index');
+console.log('sls.js working');
+module.exports = app;
--- part4/app/utils/config.js
+++ part4/app/utils/config.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-undef */
 require('dotenv').config();
+console.log(process.env.NODE_ENV);
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
-const MONGODB_NOTE_URI = process.env.MONGODB_NOTE_URI;
-const MONGODB_BLOG_URI = process.env.MONGODB_BLOG_URI;
-const MONGODB_PHONEBOOK_URI = process.env.MONGODB_PHONEBOOK_URI;
+let MONGODB_URI = process.env.MONGODB_URI;
+if (process.env.NODE_ENV === 'test') {
+    MONGODB_URI = process.env.TEST_MONGODB_URI;
+}
 
 module.exports = {
     PORT,
     MONGODB_URI,
-    MONGODB_NOTE_URI,
-    MONGODB_BLOG_URI,
-    MONGODB_PHONEBOOK_URI,
 };
+
+// const MONGODB_NOTE_URI = process.env.MONGODB_NOTE_URI;
+// const MONGODB_BLOG_URI = process.env.MONGODB_BLOG_URI;
+// const MONGODB_PHONEBOOK_URI = process.env.MONGODB_PHONEBOOK_URI;
